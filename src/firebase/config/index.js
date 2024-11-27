73db03d664d299831e4953bf1a2239e4588eb035
Sultan A. Dev <sultanabaniks@gmail.com>
2024-11-27 21:24:38 +0100

    refactor Firebase configuration to use environment variables for sensitive credentials and add .env file

--- src/firebase/config/index.js
+++ src/firebase/config/index.js
@@ -3,18 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Get Firebase credentials from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAIIAH1MNv7ZuavxR75ga622PwUQONInzs",
-  authDomain: "customer-table-badb2.firebaseapp.com",
-  projectId: "customer-table-badb2",
-  storageBucket: "customer-table-badb2.firebasestorage.app",
-  messagingSenderId: "789670405710",
-  appId: "1:789670405710:web:247e77c8526883b24e451f",
-  measurementId: "G-1S2GWZJBLZ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
- 
