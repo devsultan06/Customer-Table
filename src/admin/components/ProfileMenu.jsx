5f2111e65b9576f36c687c820e06084d1c508089
Sultan A. Dev <sultanabaniks@gmail.com>
2024-12-01 00:02:17 +0100

    feat: enhance ProfileMenu component with routing and logout functionality

--- src/admin/components/ProfileMenu.jsx
+++ src/admin/components/ProfileMenu.jsx
@@ -1,5 +1,4 @@
-// profile menu component
-import React, { useState,}from "react";
+import React, { useState } from "react";
 import {
   Typography,
   Menu,
@@ -13,18 +12,20 @@ import {
   PowerIcon,
 } from "@heroicons/react/24/solid";
 import { AccountCircle as AccountCircleIcon } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import useLogout from "../../hooks/useLogOut";
 
 const profileMenuItems = [
   {
     label: "My Profile",
     icon: UserCircleIcon,
+    to: "/admin/profile",
   },
-
   {
     label: "Inbox",
     icon: InboxArrowDownIcon,
+    to: "/admin/inbox",
   },
-
   {
     label: "Sign Out",
     icon: PowerIcon,
@@ -36,41 +37,51 @@ export function ProfileMenu() {
 
   const closeMenu = () => setIsMenuOpen(false);
 
+  const { handleLogout } = useLogout();
+
   return (
     <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
       <MenuHandler className="cursor-pointer">
-        <AccountCircleIcon
-          sx={{
-            fontSize: "35px",
-          }}
-        />
+        <AccountCircleIcon sx={{ fontSize: "35px" }} />
       </MenuHandler>
       <MenuList className="p-1">
-        {profileMenuItems.map(({ label, icon }, key) => {
+        {profileMenuItems.map(({ label, icon, to }, key) => {
           const isLastItem = key === profileMenuItems.length - 1;
-          return (
+          return isLastItem ? (
             <MenuItem
               key={label}
-              onClick={closeMenu}
-              className={`flex items-center gap-2 rounded ${
-                isLastItem
-                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                  : ""
-              }`}
+              onClick={handleLogout}
+              className="flex items-center gap-2 rounded hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
             >
               {React.createElement(icon, {
-                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                className: "h-4 w-4 text-red-500",
                 strokeWidth: 2,
               })}
               <Typography
                 as="span"
                 variant="small"
                 className="font-normal"
-                color={isLastItem ? "red" : "inherit"}
+                color="red"
               >
                 {label}
               </Typography>
             </MenuItem>
+          ) : (
+            <Link to={to} key={label} onClick={closeMenu} className="block">
+              <MenuItem className="flex items-center gap-2 rounded">
+                {React.createElement(icon, {
+                  className: "h-4 w-4",
+                  strokeWidth: 2,
+                })}
+                <Typography
+                  as="span"
+                  variant="small"
+                  className="font-normal text-inherit"
+                >
+                  {label}
+                </Typography>
+              </MenuItem>
+            </Link>
           );
         })}
       </MenuList>
